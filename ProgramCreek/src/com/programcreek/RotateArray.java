591e49f382bd3d6f51fcf0ebdeee25886e11926c
Vamshi Krishna N S <vamshi.krishna588@gmail.com>
2015-08-11 11:51:27 -0600

    1 Rotate Array in Java

--- ProgramCreek/src/com/programcreek/RotateArray.java
+++ ProgramCreek/src/com/programcreek/RotateArray.java
@@ -3,20 +3,24 @@ package com.programcreek;
 public class RotateArray {
 
 	public static void main(String[] args) {
-		int n = 10, k = 3;
+		int n = 7, k = 3;
 		int[] arr = new int[n];
 		int[] new_arr = new int[n];
+		
+		// [1,2,3,4,5,6,7]
 		for (int i = 1; i <= n; i++) {
 			arr[i - 1] = i;
 		}
 
 		int j = k;
 
+		// [5,6,7,0,0,0,0]
 		for (int i = 0; i < 3 && j > 0; i++) {
 			new_arr[i] = arr[n - j];
 			j--;
 		}
 		
+		// [5,6,7,1,2,3,4]
 		for (int i = k, p = 0; i < n; i++) {
 			new_arr[i] = arr[p];
 			p++;
