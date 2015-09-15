e9b5df9f8b3c85e1299e43075e8e7e8a94742453
Vamshi Krishna N S <vamshi.krishna588@gmail.com>
2015-09-14 23:37:15 -0600

    3 Isomorphic Strings

--- ProgramCreek/src/com/programcreek/IsomorphicStrings.java
+++ ProgramCreek/src/com/programcreek/IsomorphicStrings.java
@@ -7,31 +7,36 @@ public class IsomorphicStrings {
 
 	public static void main(String[] args) {
 		String s = "egg";
-		String t = "add";
+		String t = "adt";
 		HashMap<Character, Character> mapStoT = null;
 		HashMap<Character, Character> mapTtoS = null;
 		if (s.length() == t.length()) {
 			mapStoT = generateMap(s, t);
 			mapTtoS = generateMap(t, s);
+			System.out.println(mapStoT);
+			System.out.println(mapTtoS);
+			if(checkIsomorphic(mapStoT, mapTtoS, s) == checkIsomorphic(mapTtoS, mapStoT, t))
+				System.out.println("They are isomorphic!");
+			else
+				System.out.println("They are not isomorphic!");
+		} else {
+			System.out.println("They are not isomorphic!");
 		}
-		System.out.println(mapStoT);
-		System.out.println(mapTtoS);
-		checkIsomorphic(mapStoT, mapTtoS);
+		
 	}
 
-	private static void checkIsomorphic(HashMap<Character, Character> mapStoT,
-			HashMap<Character, Character> mapTtoS) {
-		for (int i = 0; i < mapStoT.size(); i++) {
-
-			for (Entry<Character, Character> entry1 : mapStoT.entrySet()) {
-				// System.out.println("Key : " + entry.getKey() + " Value : " +
-				// entry.getValue());
-				for (Entry<Character, Character> entry2 : mapTtoS.entrySet()) {
-					//TODO
-				}
+	private static boolean checkIsomorphic(HashMap<Character, Character> mapStoT,
+			HashMap<Character, Character> mapTtoS, String s) {
+		boolean flag = true;
+		for (int i = 0; i < s.length(); i++) {
+			if(Character.toString(s.charAt(i)).equals(Character.toString(mapTtoS.get(mapStoT.get(s.charAt(i)))))){
+			} else {
+				System.out.println(s.charAt(i) + ", " + mapStoT.get(s.charAt(i)) + ", " + mapTtoS.get(mapStoT.get(s.charAt(i))) + " are not equal!");
+				flag = false;
+				break;
 			}
 		}
-
+		return flag;
 	}
 
 	private static HashMap<Character, Character> generateMap(String s1,
